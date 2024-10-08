3f2e6e98ad447f9f38578278de75479fa227af7c
Elena Ollivin <fortnokses@gmail.com>
2024-10-08 08:42:05 +0300

    The file has been modified according to the requirements.

---
+++ jspr-code-master/01_web/http-server/src/main/java/ru/netology/Main.java
@@ -0,0 +1,16 @@
+package ru.netology;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Server server = new Server(64);
+        server.start(9999);
+    }
+}
+
